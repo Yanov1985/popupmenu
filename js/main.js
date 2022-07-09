90222145968ef4f7f5d28cd30ca5426ab0d6846b
HardyGuardy <yanov.kirill@gmail.com>
2022-07-09 16:12:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+const allTours  = document.querySelector('.service-menu_header-link')
+const popUp  = document.querySelector('.sr-portal-container')
+const cross = document.querySelector('.cross')
+const frog = document.querySelector('body')
+
+allTours.addEventListener('click', function() {
+
+	popUp.classList.add('visibility-on')
+	frog.classList.add('frog')
+	
+});
+
+cross.addEventListener('click', function() {
+
+	popUp.classList.remove('visibility-on')
+	frog.classList.remove('frog')
+
+});
